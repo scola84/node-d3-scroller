b09fb466069e65712933ac10bf1517ad91da6b7b
M.C. van den Boogaart <scola@kvdb.name>
2016-09-29 21:49:19 +0200

    Update scripts

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,10 @@ import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
+  dest: './dist/d3-scroller.js',
   entry: 'index.js',
   format: 'umd',
-  globals: {
-    'd3-selection': 'd3',
-    'd3-selection-multi': 'd3'
-  },
+  moduleName: 'd3',
   plugins: [
     resolve({
       jsnext: true
